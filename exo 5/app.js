ec8a4dec35040513fdcd420ca4b9ff620a51bcc1
ChloeArd <chloe.ardoise@gmail.com>
2020-11-25 16:38:58 +0100

    Exercice 57

--- exo 5/app.js
+++ exo 5/app.js
@@ -1,1 +1,7 @@
-let nombres = prompt("Ecrivez 2 nombres à virgules");
+let nombre1 = prompt("Ecrivez un nombre à virgule");
+let nombre2 = prompt("Ecrivez un autre nombre à virgule")
+
+let noombre1 = parseInt(Math.trunc(nombre1));
+let resultat = noombre1 * nombre2;
+
+alert("Le résultat est  " + resultat);
